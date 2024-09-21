1d08416e059ff897c656a3e75e64a1112ce17e9c
Harish7775 <harish77sonkusare@gmail.com>
2024-09-21 20:59:21 +0530

    chnages on database connection

--- src/models/index.js
+++ src/models/index.js
@@ -7,8 +7,13 @@ const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
-  dialectModule: require('pg'),
-  logging: console.log,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+  logging: false,
   pool: {
     max: 10,
     min: 0,
