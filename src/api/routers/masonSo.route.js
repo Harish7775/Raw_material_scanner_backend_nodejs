fd5338b48b072537ac1191043a94da3aa0ada923
Harish7775 <harish77sonkusare@gmail.com>
2024-10-23 17:13:19 +0530

    add validator in masonso router

--- src/api/routers/masonSo.route.js
+++ src/api/routers/masonSo.route.js
@@ -1,15 +1,15 @@
 const express = require('express')
 const masonSoController = require('../controllers/masonSoDetail.controller');
 const validateRole = require('../middleware/role');
-// const { validate } = require('../../helper/customValidation');
-// const { createCategorySchema, updateCategorySchema } = require('../validators/category.validator');
-// const { getRecordsSchema, idSchema } = require('../validators/common.validator');
+const { validate } = require('../../helper/customValidation');
+const { createMasonSoDetailSchema, masonIdSchema } = require('../validators/masonSo.validator');
+const { idSchema } = require('../validators/common.validator');
 const router = express.Router();
 
 
-router.post('/createMasonSoDetail', validateRole(["Admin", "Retailer"]), masonSoController.createMasonSoDetail);
+router.post('/createMasonSoDetail', validateRole(["Admin", "Retailer"]), validate(createMasonSoDetailSchema, 'body'), masonSoController.createMasonSoDetail);
 router.get('/getAllMasonSoDetails', validateRole(["Admin", "Retailer"]), masonSoController.getAllMasonSoDetails);
-router.get('/getMasonSoDetailById/:id', validateRole(["Admin", "Retailer"]), masonSoController.getMasonSoDetailById);
-router.get('/getTotalRewardPointsForMason/:masonId', validateRole(["Admin", "Retailer"]), masonSoController.getTotalRewardPointsForMason);
+router.get('/getMasonSoDetailById/:id', validateRole(["Admin", "Retailer"]), validate(idSchema, 'params'), masonSoController.getMasonSoDetailById);
+router.get('/getTotalRewardPointsForMason/:masonId', validateRole(["Admin", "Retailer"]), validate(masonIdSchema, 'params'), masonSoController.getTotalRewardPointsForMason);
 
 module.exports = router;
